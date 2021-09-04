55202002e0fae6ccdef54924b816c5001217e199
Roberto Ruiz Cai <robertoruizcai@gmail.com>
2021-09-04 16:53:44 -0300

    delecao dos pods

--- Pods/Target Support Files/Pods-viperAnimacao-viperAnimacaoUITests/Pods-viperAnimacao-viperAnimacaoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_viperAnimacao_viperAnimacaoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_viperAnimacao_viperAnimacaoUITests
-@end
